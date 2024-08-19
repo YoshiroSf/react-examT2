9c49f0eadb99a35021a925a144a4e677dd0f8030
Abel <I202406054@cibertec.edu.pe>
2024-08-19 22:18:58 +0000

    creacion encabezado

---
+++ TodoTurism/src/componente/encabezado.jsx
@@ -0,0 +1,13 @@
+import { Fragment } from "react"
+
+function header(){
+    return(
+        <>
+         <header>
+            <div class="logo-container">
+                <img src="ruta-al-logo.png" alt="Logo" class="logo">
+            </div>
+         </header>
+        </>
+    )
+}
