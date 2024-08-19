6792b98bc2ec7b2be75cdb1c1eff59d0ffccf077
Abel <I202406054@cibertec.edu.pe>
2024-08-19 22:49:01 +0000

    cambios enca

--- TodoTurism/src/App.jsx
+++ TodoTurism/src/App.jsx
@@ -1,11 +1,10 @@
 import { Fragment } from "react";
 import Mensaje from './componente/mensaje';
-import header from './componente/encabezado';
+import cabeza from './componente/encabezado';
 
 function app(){
   return <Mensaje />
-  return <header
-   />
+  return <header />
 }
 
 export default app;
