14c32be8bfdca7c0a3a14af37b267eb67856c8ab
Abel <I202406054@cibertec.edu.pe>
2024-08-19 22:56:00 +0000

    cavezsa

--- TodoTurism/src/App.jsx
+++ TodoTurism/src/App.jsx
@@ -1,7 +1,9 @@
 import { Fragment } from "react";
+import cabeza from "./componente/encabezado";
 
 function app(){
   return <Mensaje />
+  return <cabeza imagen="https://humanidades.com/wp-content/uploads/2017/03/ciudad-3-e1565900111723.jpg"/>
 }
 
 export default app;
