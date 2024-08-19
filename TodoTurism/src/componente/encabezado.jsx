722e5ef4b5d5d3a1b3f9d6dfe573f58eaecb4e62
Abel <I202406054@cibertec.edu.pe>
2024-08-19 22:24:56 +0000

    cambios encabezado

--- TodoTurism/src/componente/encabezado.jsx
+++ TodoTurism/src/componente/encabezado.jsx
@@ -3,11 +3,9 @@ import { Fragment } from "react"
 function header(){
     return(
         <>
-         <header>
             <div class="logo-container">
                 <img src=".assets/logo1.png" alt="" />
             </div>
-         </header>
         </>
     )
 }
